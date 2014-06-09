674767c717f4eacabcc80c7aead3772ac5d99a40
Walker Crouse <walkercrouse@hotmail.com>
2014-06-09 16:13:18 -0400

    demo
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -16,13 +16,19 @@
     </head>
     <body>
 
-        <a href="https://github.com/windy1/filter.js"><img style="position: absolute; top: 0; right: 0; border: 0;" 
-           src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" 
-           alt="Fork me on GitHub" 
-           data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png">
+        <!-- GitHub ribbon -->
+        <a href="https://github.com/windy1/filter.js">
+            <img style="position: absolute; top: 0; right: 0; border: 0;" 
+                 src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" 
+                 alt="Fork me on GitHub" 
+                 data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png">
         </a>
 
+        <div class="fjs-nav">
+        </div>
+
         <div class="container">
+            <!-- Header -->
             <div id="header">
                 <h1 class="title">Filter.js</h1>
 
@@ -32,51 +38,52 @@
                 you to filter paginated data that is not loaded on the client yet.</p>
             </div>
 
-            <div class="content">
-                <h2 class="title" style="margin-bottom: 0px;">Demos</h2>
+            <!-- Demo -->
+            <h2 class="title" style="margin-bottom: 0px;">Demo</h2>
 
-                <p class="summary" style="text-align: center; margin-bottom: 20px;">
-                    (made in conjunction with twitter bootstrap and 
-                    <a href="https://github.com/windy1/more.js">more.js</a>)
-                </p>
+            <p class="summary" style="text-align: center; margin-bottom: 20px;">
+                (made in conjunction with twitter bootstrap and 
+                <a href="https://github.com/windy1/more.js">more.js</a>)
+            </p>
 
-                <div class="panel panel-primary" style="width: 50%; margin-left: auto; margin-right: auto;">
-                    <div class="panel-heading">
-                        <input type="text" placeholder="Filter employees&hellip;" class="form-control filter">
-                    </div>
-                    <div class="panel-body">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Last name</th>
-                                    <th>First name</th>
-                                    <th>Middle initial</th>
-                                    <th>Gender</th>
-                                    <th>Age</th>
-                                </tr>
-                            </thead>
-                            <tbody class="employees">
-                                <?php
-                                $employees = Employee::paginate(5);
-                                foreach ($employees as $employee) {
-                                    echo '<tr>'.PHP_EOL
-                                       . '<td>'.$employee->last_name.'</td>'.PHP_EOL
-                                       . '<td>'.$employee->first_name.'</td>'.PHP_EOL
-                                       . '<td>'.$employee->middle_initial.'</td>'.PHP_EOL
-                                       . '<td>'.$employee->gender.'</td>'.PHP_EOL
-                                       . '<td>'.$employee->age.'</td>'.PHP_EOL;
-                                }
-                                ?>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="panel-footer">
-                        <button class="more btn btn-default btn-lg btn-block" data-loading-text="Loading&hellip;">
-                            More
-                        </button>
-                    </div>
+            <div class="panel panel-primary" style="width: 50%; margin-left: auto; margin-right: auto;">
+                <div class="panel-heading">
+                    <input type="text" placeholder="Filter employees&hellip;" class="form-control filter">
                 </div>
+                <div class="panel-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Last name</th>
+                                <th>First name</th>
+                                <th>Middle initial</th>
+                                <th>Gender</th>
+                                <th>Age</th>
+                            </tr>
+                        </thead>
+                        <tbody class="employees">
+                            <?php
+                            $employees = Employee::paginate(5);
+                            foreach ($employees as $employee) {
+                                echo '<tr>'.PHP_EOL
+                                   . '<td>'.$employee->last_name.'</td>'.PHP_EOL
+                                   . '<td>'.$employee->first_name.'</td>'.PHP_EOL
+                                   . '<td>'.$employee->middle_initial.'</td>'.PHP_EOL
+                                   . '<td>'.$employee->gender.'</td>'.PHP_EOL
+                                   . '<td>'.$employee->age.'</td>'.PHP_EOL;
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="panel-footer">
+                    <button class="more btn btn-default btn-lg btn-block" data-loading-text="Loading&hellip;">
+                        More
+                    </button>
+                </div>
+            </div>
 
+<!-- Demo code snippet -->
 <pre class="prettyprint lang-javascript" style="width: 50%; margin-left: auto; margin-right: auto;">
   var currentPage = 1;
 
@@ -92,10 +99,120 @@
   });
 </pre>
 
-            <div class="divider" style="color: gray;">Copyright &copy; 2014 Walker Crouse</div>
-        </div>
+            <div style="text-align: center;">
+                <a href="view-source:{{URL::to('/')}}" target="_blank">View full source</a>
+            </div>
 
-        <div id="map" style="display: hidden;"></div>
+            <div class="divider"></div>
+
+            <!-- Tutorial -->
+            <div class="tutorial">
+                <h2 class="title">Getting Started</h2>
+                <p>Setup your HTML table markup</p>
+            </div>
+
+<pre class="prettyprint lang-html code">
+  &lt;input type="text" id="filter" placeholder="Filter by name&hellip;"&gt;
+  &lt;table&gt;
+      &lt;thead&gt;
+          &lt;td&gt;&lt;th&gt;#&lt;/th&gt;&lt;/td&gt;
+          &lt;td&gt;&lt;th&gt;Orchard&lt;/th&gt;&lt;/td&gt;
+          &lt;td&gt;&lt;th&gt;Apples&lt;/th&gt;&lt;/td&gt;
+          &lt;td&gt;&lt;th&gt;Oranges&lt;/th&gt;&lt;/td&gt;
+          &lt;td&gt;&lt;th&gt;Pears&lt;/th&gt;&lt;/td&gt;
+      &lt;/thead&gt;
+      &lt;tbody id="orchards"&gt;&lt;/tbody&gt;
+  &lt;/table&gt;
+</pre>
+
+            <div class="tutorial pull-down"><p>Add jquery.filter.js and jquery.(min).js to your template</p></div>
+
+<pre class="prettyprint lang-html code">
+  &lt;script src="jquery.min.js"&gt;&lt;/script&gt;
+  &lt;script src="jquery.filter.js"&gt;&lt;/script&gt;
+</pre>
+
+            <div class="tutorial pull-down">
+                <p>Bind your input field using the <code class="prettyprint lang-javascript">filter(Object)</code> 
+                function.</p>
+            </div>
+
+<pre class="prettyprint lang-javascript code">
+$("#filter").filter({
+  // URL to filter resources from. 
+  url: "http://example.com/resources/json", 
+
+  // AJAX dataType. Default: "json".
+  dataType: "json",
+
+  // table to populate, does not actually have to be a &lt;table&gt; element.
+  table: "#orchards",
+
+  // Parameter that determines the results of the filter. Default: "q".
+  queryParam: "orchard_name",
+
+  // Parameter that paginates the data. Default: "page".
+  pageParam: "p", // defaults to 'page'
+
+  // callback called before the AJAX request is sent.
+  before: function(request) {
+      ...
+  },
+
+  // callback called after AJAX request is received, regardless of the result.
+  after: function() {
+      ...
+  },
+
+  // callback to build HTML from the returned data.
+  buildEntries: function(json) {
+      var html = ...
+      return html;
+  }
+});
+</pre>
+
+            <div class="tutorial pull-down">
+                <p>Integrate with <a href="https://github.com/windy1/more.js">more.js</a> (optional)</p>
+            </div>
+
+<pre class="prettyprint lang-javascript code">
+&lt;button class="btn btn-lg btn-default btn-block" id="more" 
+           data-loading-text="Loading&hellip;"&gt;
+
+$("#more").more({
+    url: "http://example.com/resources/json",
+    table: "#orchards",
+
+    ...
+
+    // Set this to false if you are not using twitter bootstrap's "loading" 
+    // button plugin.
+    bootstrap: true
+});
+</pre>
+
+            <div class="divider"></div>
+
+            <h2 class="title">Download</h2>
+
+            <div style="text-align: center;">
+                <div>
+                    <a href="https://raw.githubusercontent.com/windy1/filter.js/master/jquery.filter.js" 
+                       class="btn btn-lg btn-primary" style="width: 200px;" target="_blank">
+                        Download
+                    </a>
+                </div>
+                <div class="pull-down">
+                    <a href="https://github.com/windy1/filter.js" class="btn btn-lg btn-primary" style="width: 200px"
+                       target="_blank">
+                        View on GitHub
+                    </a>
+                </div>
+            </div>
+
+            <div class="divider">&copy; {{date('Y')}} Walker Crouse</div>
+        </div>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
